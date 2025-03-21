4df05e0bb211ce65358410a02b5a1876641c1943
Prince Saxena <princesaxena202020@gmail.com>
2025-03-21 16:51:42 +0530

    Assignment

---
+++ Assignment1.py
@@ -0,0 +1,8 @@
+
+a = int(input("Enter first number :"))
+b = int(input("/nEnter second number :"))
+
+print("Addition :",a+b)
+print("Subtraction :",(a-b))
+print("Division :",(a/b))
+print("Multiplication :",(a*b))
